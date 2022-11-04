88057cfd050da90690076b92a4650eddc7454dd7
Alioune Seye <aliouneseye@aliounes-macbook-pro.home>
2022-11-04 12:20:32 +0100

    rescaled all potentials from version 0 to 9 to be ranging up to 1 meV

--- mag_vec_fem/plot_pot.py
+++ mag_vec_fem/plot_pot.py
@@ -2,8 +2,8 @@ from exploit_fun import *
 from matplotlib import pyplot as plt
 res_path=res_path_f()
 
-lnm=200
-pot_version=2
+lnm=1000
+pot_version=0
 nameV=res_path+'/pre_interp_pot/'+'pre_interp_potv'+str(pot_version)+'l'+str(lnm)+'E1eVx15.npy'
 V_preinterp=np.load(nameV)
 
--- mag_vec_fem/tests.py
+++ mag_vec_fem/tests.py
@@ -4,18 +4,23 @@ from fem_base.graphics import *
 from fem_base.potentials import interpolate_pot
 from exploit_fun import *
 
-pot_version=5
+pot_version=0
 lnm=500
 h=0.005
 gauge='Sym'
 l=lnm*10**-9
 N_eig=100
 res_path=res_path_f()
-
-nameV=res_path+'/pre_interp_pot/'+'pre_interp_potv'+str(pot_version)+'l'+str(lnm)+'E1eVx15.npy'
-nameq=res_path+'/Vq/Vql'+str(lnm)+'.npy'
-Th=HyperCube(2,int(1/h),l=lnm*10**-9)
-V_preinterp=0.001*np.load(nameV)
-points=np.load(nameq)
-#V=interpolate_pot(V_preinterp,points,Th.q)
-print(np.shape(V_preinterp),np.shape(points))
+for pot_version in range(9):
+    for lnm in (50,100,200,500,1000):
+        nameV=res_path+'/pre_interp_pot/'+'pre_interp_potv'+str(pot_version)+'l'+str(lnm)+'E1eVx15.npy'
+        '''nameq=res_path+'/Vq/Vql'+str(lnm)+'.npy'
+        Th=HyperCube(2,int(1/h),l=lnm*10**-9)
+        V_preinterp=0.001*np.load(nameV)
+        points=np.load(nameq)
+        #V=interpolate_pot(V_preinterp,points,Th.q)
+        print(np.shape(V_preinterp),np.shape(points))
+        '''
+        V_unscaled=np.load(nameV)
+        V_scaled=0.001*V_unscaled
+        np.save(nameV,V_scaled)
