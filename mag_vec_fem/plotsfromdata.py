615e7bcef52e0161805ad22b86bb2a467df9b9a9
Alioune SEYE <alioune.seye@laposte.net>
2022-11-30 14:41:03 +0100

    load texlive from charon, correct paths in exploit_fun

--- mag_vec_fem/plotsfromdata.py
+++ mag_vec_fem/plotsfromdata.py
@@ -3,30 +3,30 @@ from fem_base.exploit_fun import datafile, saveplots_fromdata, data_path
 import matplotlib.pyplot as plt
 from fem_base.mesh import HyperCube
 
-res_path=data_path
+res_path='/media/ase/Transcend/Thèse/mag_vec_fem/data' #data_path
 lnm=200
 h=0.001
 gauge='Sym'
 N_eig=100
 Th=HyperCube(2,int(1/h),l=lnm*10**-9)
 
-B=1
+'''B=1
 V_maxmeV=10
 for pot_version in (7,8):
     dat_file=datafile(lnm,B,V_maxmeV,h,pot_version,gauge,N_eig)
-    name_preeig='l'+str(lnm)+'B'+str(B)+'V'+str(V_maxmeV)+'v'+str(pot_version)+'h'+str(int(1/h))
+    name_preeig='l'+str(lnm)+'B'+str(B)+'V'+str(V_maxmeV)+'v'+str(pot_version)+'h'+str(int(1/h))+gauge
     saveplots_fromdata(Th,dat_file,name_preeig)
 
 B=5
 for V_maxmeV in (1,10,100):
     for pot_version in (5,6,7,8):
         dat_file=datafile(lnm,B,V_maxmeV,h,pot_version,gauge,N_eig)
-        name_preeig='l'+str(lnm)+'B'+str(B)+'V'+str(V_maxmeV)+'v'+str(pot_version)+'h'+str(int(1/h))
-        saveplots_fromdata(Th,dat_file,name_preeig)
+        name_preeig='l'+str(lnm)+'B'+str(B)+'V'+str(V_maxmeV)+'v'+str(pot_version)+'h'+str(int(1/h))+gauge
+        saveplots_fromdata(Th,dat_file,name_preeig)'''
 B=10
 for pot_version in (7,8):
     for V_maxmeV in (1,10,100):
         for gauge in ('Sym','LandauX'):
-            dat_file=datafile(lnm,B,V_maxmeV,h,pot_version,gauge,N_eig)
-            name_preeig='l'+str(lnm)+'B'+str(B)+'V'+str(V_maxmeV)+'v'+str(pot_version)+'h'+str(int(1/h))
-            saveplots_fromdata(Th,dat_file,name_preeig)
+            dat_file=datafile(lnm,B,V_maxmeV,h,pot_version,gauge,N_eig,res_path=res_path)
+            name_preeig='l'+str(lnm)+'B'+str(B)+'V'+str(V_maxmeV)+'v'+str(pot_version)+'h'+str(int(1/h))+gauge
+            saveplots_fromdata(Th,dat_file,name_preeig, res_path=res_path, real=True)
